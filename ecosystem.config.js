f1dbd4e7ee347bb675b7c0dd40ac69a79ba58e79
Dika P. <dikapramudia11@gmail.com>
2024-06-02 20:54:21 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	apps: [
+		{
+			name: 'rails-app',
+			script: 'bundle',
+			args: 'exec puma -C config/puma.rb',
+			interpreter: '/bin/bash',
+			cwd: '/home/viapulsa/certbot/simpul-chat-server',
+			env: {
+				RAILS_ENV: 'production',
+				PORT: 3000,
+			},
+		},
+	],
+};
